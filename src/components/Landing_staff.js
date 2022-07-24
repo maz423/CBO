c06534c10706b77d2a6f81d9c64a2cdaa045e7df
Mohammad Zargar <maz423@usask.ca>
2022-07-24 12:34:41 -0600

    CSS overhaul

---
+++ src/components/Landing_staff.js
@@ -0,0 +1,26 @@
+import './Post.css'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import {Row} from 'react-bootstrap'
+import Col from 'react-bootstrap';
+
+export const Landing_staff = (props) => {
+
+return (
+
+<div>
+    <Container>
+      <Row>
+
+        
+      </Row>
+
+    </Container>
+
+</div> 
+
+
+);
+
+
+}
